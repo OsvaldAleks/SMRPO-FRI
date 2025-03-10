03d46d126dac800b0489242ab5554ed85d42a197
Aleksander <osvald.aleksander@gmail.com>
2025-03-10 08:34:05 +0100

    design of sprint detail site

--- src/pages/SprintDetails.js
+++ src/pages/SprintDetails.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useParams } from "react-router-dom";
 import { getSprintData } from '../api.js'
+import './style/SprintDetails.css'
 
 const SprintDetails = () => {
   const { user, loading } = useAuth();
@@ -34,21 +35,59 @@ const SprintDetails = () => {
 
       {sprint ? (
         <div>
-          <p><strong>Start Date:</strong>
+          <p><strong>Start Date: </strong>
                 {sprint?.start_date
                     ? sprint.start_date
                     : "No end date available"}
             </p>
-          <p><strong>End Date:</strong>
+          <p><strong>End Date: </strong>
                 {sprint?.end_date
                     ? sprint.end_date
                     : "No end date available"}
             </p>
-          <p><strong>Velocity:</strong> {sprint.velocity}</p>
+          <p><strong>Velocity: </strong> {sprint.velocity}</p>
         </div>
       ) : (
         <p>Loading sprint data...</p>
       )}
+    <div className="responsive-table-container-sprints">
+      <table className="responsive-table">
+        <thead>
+          <tr>
+            <th>TODO</th>
+            <th>Doing</th>
+            <th>Done</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className="userStory">
+                <p>Name of story</p>
+                <p>Must have</p>
+                <p>Business value: 23</p>
+              </div>
+            </td>
+            <td>
+              <div className="userStory">
+                <p>Another story</p>
+                <p>Nice to have</p>
+                <p>Business value: 10</p>
+              </div>
+            </td>
+            <td>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div className="userStory plus-button">+</div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+
     </div>
   );
 };
