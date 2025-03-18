fb4ab4404bc0c2b5677cbe6bd10bbb9a5a207d53
Aleksander <osvald.aleksander@gmail.com>
2025-03-18 18:25:22 +0100

    site documentation styling

--- src/pages/Documentation.js
+++ src/pages/Documentation.js
@@ -9,104 +9,103 @@ const Documentation = () => {
       <h1>User Documentation</h1>
       {user && user.system_rights === "Admin" && (
         <>
-        <h2>Admin documentation</h2>
-        <details>
-          <summary>Add a new user</summary>
-          <p>Click on the <strong>Manage Users</strong> tab in the navigation bar.</p>
-          <p>Click on the <strong>Add User</strong> button.</p>
-          <p>Fill in the user's details:</p>
-          <ul>
-            <li>A unique username</li>
-            <li>Email</li>
-            <li>First Name</li>
-            <li>Last Name</li>
-            <li>Password</li>
-            <li>System Rights</li>
-          </ul>
-          <p>Click <strong>Add User</strong>.</p> 
-        </details>
-        <details>
-          <summary>Create a new project</summary>
-          <p>Click on the <strong>My Projects</strong> tab in the navigation bar.</p>
-          <p>Click on the <strong>+</strong> button.</p>
-          <p>Select a name for the project and assign roles to selected members.</p>
-          <p>After you've selected at least one:</p>
-            <ul>
+          <h2>Admin documentation</h2>
+          <details>
+            <summary>Add a new user</summary>
+            <ol>
+              <li>Click on the <strong>Manage Users</strong> tab in the navigation bar.</li>
+              <li>Click on the <strong>Add User</strong> button.</li>
+              <li>Fill in the user's details:</li>
+              <ul>
+                <li>A unique username</li>
+                <li>Email</li>
+                <li>First Name</li>
+                <li>Last Name</li>
+                <li>Password</li>
+                <li>System Rights</li>
+              </ul>
+              <li>Click <strong>Add User</strong>.</li>
+            </ol>
+          </details>
+          <details>
+            <summary>Create a new project</summary>
+            <ol>
+              <li>Click on the <strong>My Projects</strong> tab in the navigation bar.</li>
+              <li>Click on the <strong>+</strong> button.</li>
+              <li>Select a name for the project and assign roles to selected members.</li>
+              <li>Ensure you have assigned at least one of each:</li>
+              <ul>
                 <li>Product Manager</li>
                 <li>SCRUM Master</li>
                 <li>Developer</li>
-            </ul>
-          <p>Click <strong>Create Project</strong>.</p>
-        </details>
-        <h2>General user documentation</h2>
+              </ul>
+              <li>Click <strong>Create Project</strong>.</li>
+            </ol>
+          </details>
+          <h2>General user documentation</h2>
         </>
       )}
       {user ? (
         <>
-        <details>
-          <summary>Create a sprint</summary>
-          <p>You need to be a <strong>SCRUM master</strong> in order to create a new Sprint.</p>
-          <p>Within a project click on the <strong>+</strong> button under the <strong>Sprints</strong> section.</p>
-          <p>Determine the start and end dates of the sprint along with your sprint speed and hit <strong>ADD SPRINT</strong>.</p>
-        </details>
-        <details>
-          <summary>Create a User Story</summary>
-            <p>You need to be a <strong>SCRUM master</strong> or a <strong>Product Manager</strong> in order to create a new Sprint.</p>
-            <p>Within a project click on the <strong>+</strong> button under the <strong>User Stories</strong> section.</p>
-            <p>Determine a:</p>
-            <ul>
+          <details>
+            <summary>Create a sprint</summary>
+            <p><em>You need to be a SCRUM master to create a new Sprint.</em></p>
+            <ol>
+              <li>Within a project, click on the <strong>+</strong> button under the <strong>Sprints</strong> section.</li>
+              <li>Determine the start and end dates of the sprint along with your sprint speed.</li>
+              <li>Click <strong>ADD SPRINT</strong>.</li>
+            </ol>
+          </details>
+          <details>
+            <summary>Create a User Story</summary>
+            <p><em>You need to be a SCRUM master or a Product Manager to create a new User Story.</em></p>
+            <ol>
+              <li>Within a project, click on the <strong>+</strong> button under the <strong>User Stories</strong> section.</li>
+              <li>Fill in the following details:</li>
+              <ul>
                 <li>Unique name for your user story</li>
-                <li>enter a description</li>
-                <li>a list of acceptance criteria</li>
-                <li>select a Priority and</li>
+                <li>Description</li>
+                <li>List of acceptance criteria</li>
+                <li>Priority selection</li>
                 <li>Business Value</li>
-            </ul>
-            <p>then hit <strong>Add User Story</strong>.</p>
-        </details>
-        <details>
+              </ul>
+              <li>Click <strong>Add User Story</strong>.</li>
+            </ol>
+          </details>
+          <details>
             <summary>Determine a Story Point value of a User Story</summary>
-            <p>You need to be a <strong>SCRUM master</strong> in order to set a Story Point value.</p>
-            <p>You <strong>cannot edit the Story Point value of a Story in an active Sprint</strong>.</p>
-            <p>Click on the <strong>User Story</strong> you want to set a Story Point value for.</p>
-            <p><strong>Enter a Story Point value</strong> and hit <strong>✔</strong> to save your changes.</p>
-        </details>
-        <details>
-            <summary>Add story to a Sprint</summary>
-            <p>You need to be a <strong>SCRUM master</strong> in order to add a story to a sprint.</p>
-            <p>In the Sprint overview click the <strong>+</strong> button.</p>
-            <p>Select the Stories you wish to include by clicking the <strong>checkbox</strong>.</p>
-            <p>Submit your selection by clicking the <strong>ADD SELECTED -></strong> button.</p>
-        </details>
-        <details>
+            <p><em>You need to be a SCRUM master to set a Story Point value.</em></p>
+            <p><em>You cannot edit the Story Point value of a Story in an active Sprint.</em></p>
+            <ol>
+              <li>Click on the <strong>User Story</strong> you want to set a Story Point value for.</li>
+              <li>Enter a Story Point value.</li>
+              <li>Click <strong>✔</strong> to save your changes.</li>
+            </ol>
+          </details>
+          <details>
+            <summary>Add a story to a Sprint</summary>
+            <p><em>You need to be a SCRUM master to add a story to a sprint.</em></p>
+            <ol>
+              <li>In the Sprint overview, click the <strong>+</strong> button.</li>
+              <li>Select the Stories you wish to include by clicking the <strong>checkbox</strong>.</li>
+              <li>Click <strong>ADD SELECTED →</strong>.</li>
+            </ol>
+          </details>
+          <details>
             <summary>Add a task to a User Story</summary>
-            <p>You need to be a <strong>SCRUM master</strong> or a <strong>developer</strong> in order to add a task to a User Story.</p>
-            <p>A story needs to be <strong>in a sprint</strong> to have tasks.</p>
-            <p>In the Story overview click the <strong>+ ADD SUBTASK</strong> button.</p>
-            <p>Input data:</p>
-            <ul>
+            <p><em>You need to be a SCRUM master or a developer to add a task.</em></p>
+            <p><em>The story must be in a sprint.</em></p>
+            <ol>
+              <li>In the Story overview, click the <strong>+ ADD SUBTASK</strong> button.</li>
+              <li>Fill in the following details:</li>
+              <ul>
                 <li>Task name</li>
                 <li>Time estimate</li>
-                <li><italic>optionally you can suggest a </italic>Developer.</li>
-            </ul>
-            <p>Confirm the task creation by clicking the <strong>SAVE SUBTASK</strong> button.</p>
-        </details>
-        <details>
-          <summary>Claim a task</summary>
-          <p>You need to be a <strong>developer</strong> in order to claim a Task.</p>
-          <p>In the Story overview under the Subtasks rubric claim a task, by selecting the <strong>checkmark</strong> in the Claim column.</p>
-        </details>
-        <details>
-          <summary>Unclaim a task</summary>
-          <p>You need to be a <strong>developer</strong> in order to claim a Task.</p>
-          <p>The task must be currently <strong>claimed by you</strong> for you to be able to unclaim it.</p>
-          <p>In the Story overview under the Subtasks rubric claim a task, by unselecting the <strong>checkmark</strong> in the Claim column.</p>
-        </details>
-        <details>
-          <summary>Mark a task as completed</summary>
-          <p>You need to be a <strong>developer</strong> in order to claim a Task.</p>
-          <p>The task must be currently <strong>claimed by you</strong> for you to be able to mark it as completed.</p>
-          <p>In the Story overview under the Subtasks rubric mark a task as completed, by selecting the <strong>checkmark</strong> in the Done column.</p>
-        </details>
+                <li><em>Optionally, suggest a Developer.</em></li>
+              </ul>
+              <li>Click <strong>SAVE SUBTASK</strong>.</li>
+            </ol>
+          </details>
         </>
       ) : (
         <details>
@@ -115,8 +114,11 @@ const Documentation = () => {
             <img src="/assets/documentation/login in navbar.png" alt="login in navbar" />
             <img src="/assets/documentation/login form.png" alt="login form" />
           </div>
-          <p>Navigate to the <strong>login</strong> page via the navigation bar.</p>
-          <p>Input your email and password and hit <strong>Login</strong>.</p>
+          <ol>
+            <li>Navigate to the <strong>login</strong> page via the navigation bar.</li>
+            <li>Input your email and password.</li>
+            <li>Click <strong>Login</strong>.</li>
+          </ol>
         </details>
       )}
     </div>
